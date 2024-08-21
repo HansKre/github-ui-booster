483ea162597fbf663b39a6b7f5723f7b9cc7d4f7
HANS KREBS <hans.krebs@mercedes-benz.com>
2024-08-21 21:01:48 +0200

    Add comments as documentation

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -4,11 +4,11 @@ import { Settings, getSettings } from "./services";
 
 let observer: MutationObserver | null = null;
 
-const observe = () => {
-  if (!observer) return;
+const observe = (observer: MutationObserver) => {
   const tabNavigation = document.querySelector(".UnderlineNav");
   const filterInput = document.getElementById("js-issues-search");
 
+  // tabNavigation-DOM changes when GH-page-content changes, hence the need to re-run our scripts
   if (tabNavigation) {
     observer.observe(tabNavigation, {
       childList: true,
@@ -17,6 +17,7 @@ const observe = () => {
     });
   }
 
+  // warum?
   if (filterInput) {
     observer.observe(filterInput, {
       attributes: true,
@@ -29,6 +30,13 @@ getSettings({
   onError: () => alert("Couldn't load from chrome storage"),
 });
 
+/**
+ * This method of content_scripts is automatically executed on initial page load, refreshes, and full navigations.
+ *
+ * For SPAs and dynamic content, additional strategies like Mutation Observer
+ * are needed to ensure the content script is triggered when the page content
+ * changes without a full reload.
+ */
 function handleContent(settings: Settings) {
   const baseUiUrl = `${settings.ghBaseUrl.replace("/api/v3", "")}/${
     settings.org
@@ -52,6 +60,6 @@ function handleContent(settings: Settings) {
       });
     });
 
-    observe();
+    observe(observer);
   }
 }
