bdedacc4beef53910947c9d38729065ee8e9e431
HANS KREBS <hans.krebs@mercedes-benz.com>
2024-11-07 17:10:24 +0100

    Rename content_script

--- src/content_pr_page.tsx
+++ src/content_pr_page.tsx
@@ -22,7 +22,7 @@ getSettings({
 });
 
 /**
- * This method of content_scripts is automatically executed on initial page load, refreshes, and full navigations.
+ * This method is automatically executed on initial page load, refreshes, and full navigations.
  *
  * For SPAs and dynamic content, additional strategies like Mutation Observer
  * are needed to ensure the content script is triggered when the page content
@@ -32,9 +32,6 @@ function handleContentChange(settings: Settings) {
   if (window.location.href.startsWith(urls(settings).urlUiBase)) {
     if (observer) return;
 
-    // handlers take care of checking for conditions to run on their own
-    handlePrFilter(settings, settings.autoFilter);
-    handlePrsPage(settings);
     handlePrPage(settings);
 
     observer = new MutationObserver((mutations) => {
