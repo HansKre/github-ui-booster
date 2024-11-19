547bd6ae92186b67c5a2d310cac521abc1a5bd9c
HANS KREBS <hans.krebs@me.com>
2024-11-19 17:03:17 +0100

    Fix urls

--- src/content/utils/urls.ts
+++ src/content/utils/urls.ts
@@ -1,7 +1,7 @@
 import { InstanceConfig } from "../../services";
 
 export function urls({ ghBaseUrl, org, repo }: InstanceConfig) {
-  const urlUiBase = `${ghBaseUrl.replace("/api/v3", "")}/${org}/${repo}`;
+  const urlUiBase = `${getUrlUiBase(ghBaseUrl)}/${org}/${repo}`;
   const urlUiPrs = `${urlUiBase}/pulls`;
   const urlUiPr = `${urlUiBase}/pull/`;
 
@@ -10,4 +10,8 @@ export function urls({ ghBaseUrl, org, repo }: InstanceConfig) {
     urlUiPrs,
     urlUiPr,
   };
+}
+
+export function getUrlUiBase(ghBaseUrl: string) {
+  return ghBaseUrl.replace("/api/v3", "").replace("api.", "");
 }
