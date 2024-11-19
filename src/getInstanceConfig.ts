9f95218a1a79ea69531dba8272cd8dc1c481c609
HANS KREBS <hans.krebs@me.com>
2024-11-19 17:03:41 +0100

    Add support for multiple orgs and repos

--- src/getInstanceConfig.ts
+++ src/getInstanceConfig.ts
@@ -1,9 +1,31 @@
-import { Settings } from "./services";
+import { getUrlUiBase } from "./content/utils/urls";
+import { InstanceConfig, Settings } from "./services";
 
-export function getInstanceConfig(settings: Settings) {
-  return settings.instances.find(({ ghBaseUrl }) =>
-    window.location.href.includes(
-      ghBaseUrl.replace("/api/v3", "").replace("api.", "")
-    )
+export function getInstanceConfig(
+  settings: Settings
+): InstanceConfig | undefined {
+  const instance = settings.instances.find(({ ghBaseUrl }) =>
+    window.location.href.includes(getUrlUiBase(ghBaseUrl))
   );
+  if (!instance) return undefined;
+
+  const org = instance.org
+    .split(",")
+    .find((value) => window.location.href.includes(`/${value}`));
+
+  if (!org) return undefined;
+
+  const repo = instance.repo
+    .split(",")
+    .find((value) => window.location.href.includes(`/${value}`));
+
+  if (!repo) return undefined;
+
+  const instanceConfig = {
+    ...instance,
+    org,
+    repo,
+  };
+
+  return instanceConfig;
 }
