857020bbbc50c812cbb94a4d62ddf044b72880e7
HANS KREBS <hans.krebs@mercedes-benz.com>
2024-11-24 13:23:11 +0100

    Fix cursor in ConflictsHint

--- src/components/ConflictsHint/ConflictsHint.tsx
+++ src/components/ConflictsHint/ConflictsHint.tsx
@@ -1,11 +1,12 @@
 import { Tooltip } from "@primer/react/next";
 import React from "react";
 import { cns } from "ts-type-safe";
+import styles from "./ConflictsHint.module.scss";
 
 export const ConflictsHint: React.FC = () => {
   return (
     <Tooltip text="The base branch contains changes which need manual conflict resolution">
-      <span className={cns("color-fg-muted")}>☣️</span>
+      <span className={cns("color-fg-muted", styles.icon)}>☣️</span>
     </Tooltip>
   );
 };
